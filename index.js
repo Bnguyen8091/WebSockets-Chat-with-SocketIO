a2189061eab1cfe5f974ad34c049e366353c6282
Bnguyen8091 <nguyenbrian562@gmail.com>
2025-11-07 17:44:58 -0500

    Initial WebSocket chat assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(http, { cors: { origin: '*' } });
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
